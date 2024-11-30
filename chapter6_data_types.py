7bb2a30dfd2e503a734e0572b8706edda39b2838
Ibaghouchene Juba <ibaghouchenejubayuba@gmail.com>
2024-11-30 08:56:54 +0100

    Update chapter6_data_types.py

--- chapter6_data_types.py
+++ chapter6_data_types.py
@@ -1,268 +1,190 @@
+# There are 3 main data types discussed here:
 
-#there is 3 data types :
-
-# list : can store inside all variables types 
-# define :
-nbr_list = [1 , 2 ,3]
+# 1. Lists: Can store all variable types
+# Define a list
+nbr_list = [1, 2, 3]
 print(nbr_list)
 print(type(nbr_list))
 
-#list can have multiple variable types
-multi_vars_list = ['strings',123, False ]
+# A list can contain multiple variable types
+multi_vars_list = ['strings', 123, False]
 print(multi_vars_list)
-# the constrator methode
-constr_list = list(['gdsc',20114, True])
- 
-#operations :
-#get an element or an interval of elements :
-#first
-print(nbr_list[0])
-#last 
-print(nbr_list[-1])
-#range
-print(nbr_list[1:2])
 
-# get the index of and element:
+# Using the constructor method
+constr_list = list(['gdsc', 20114, True])
+print(constr_list)
+
+# Operations on lists:
+# Access an element or a range of elements
+print(nbr_list[0])  # First element
+print(nbr_list[-1])  # Last element
+print(nbr_list[1:2])  # Range (end index is excluded)
+
+# Get the index of an element
 print(nbr_list.index(3))
 
-#check teh existance of an element in a list : use "in" key word , the output will be a booleen var
+# Check the existence of an element in a list
 print(2 in nbr_list)
 print('hello' in nbr_list)
 
-# add an element to an existing list 
-
-    #basic methode
-nbr_list += 'hello'   
-print(nbr_list) 
-    #using the append methode
+# Add an element to a list
+# Using the append method
 nbr_list.append('run')
 print(nbr_list)
 
-    
-#add a list to an other list :
-str_list = ['hi', 'by',"hello"] # note : if you use : double quots ""  used each char is considered as an element , if you use : single quot '' all of the str will be considered as a single element 
-    #basic methode
-str_list += nbr_list    
-print(str_list)
-    #using the extend methode
-str_list.extend(nbr_list)
-print(str_list)
-#inserte an elemets in a given position in te list :
-nbr_list.insert(0 , '0') # the first position is the index and second is the element 
+# Add multiple elements to the list
+nbr_list.extend(['hello', 'world'])
 print(nbr_list)
 
-# the pop methode gets the first elemnt and delets it from the list  
+# Add a list to another list
+str_list = ['hi', 'bye', 'hello']
+str_list += nbr_list
+print(str_list)
 
-#delete a list
-del str_list
-#delete an element and its position 
-nbr_list.remove('2') #from the accual item
-del nbr_list[2] # from the position
-#get the nbr of elemenst / lengh of a list 
+# Insert an element at a given position
+nbr_list.insert(0, 'start')  # Insert at the beginning
+print(nbr_list)
+
+# Remove elements:
+   # Remove by value
+nbr_list.remove(2)  # Removes the first occurrence of 2
+    # Remove by index
+del nbr_list[2]  # Removes the element at index 2
+print(nbr_list)
+
+# Get the length of a list
 print(len(nbr_list))
 
-# clear elemnts of a list 
-nums =[1,2,3,4,56,78 ]
+# Clear all elements in a list
+nums = [1, 2, 3, 4, 56, 78]
 nums.clear()
+print(nums)
 
-#sort methode 
+# Sort a list
+nbr_list = [3, 1, 4, 2]
 nbr_list.sort()
 print(nbr_list)
-# inverse methode 
+
+# Sort in reverse order
 nbr_list.sort(reverse=True)
 print(nbr_list)
 
 
-
-
-#sets : is list but with no duplicats ; 
-# define :
+# 2. Sets: Similar to lists but do not allow duplicates
+# Define a set
 num_set = {1, 3, 2}
-numbers = { 1, 1, True, False, 0 , 2}
-print(num_set)   # the input will be in order
-print(numbers)  #note ; true is a duplicate of 1 , and false is a duplicate of 0
+numbers = {1, 1, True, False, 0, 2}
+print(num_set)  # Sets do not guarantee order
+print(numbers)  # True is a duplicate of 1, and False is a duplicate of 0
 print(type(numbers))
 print(len(num_set))
 print(len(numbers))
-#methodes :sets have the same methodes as lists
+
+# Sets share some methods with lists, but elements cannot be accessed by index.
 
 
-# tuples data cant be modified 
-tupl = tuple((1 , 2 , 3)) # this called paking a tuple unpaking is expalained below
-tuple1 = ("hello" , 22 , True)
+# 3. Tuples: Immutable (cannot be modified)
+# Define a tuple
+tupl = tuple((1, 2, 3))
+tuple1 = ("hello", 22, True)
 
 print(type(tupl))
-print(isinstance(tuple1 , tuple))
+print(isinstance(tuple1, tuple))
 
-# add an element to the tuple 
-
+# Add elements to a tuple (requires converting to a list first)
 tubl_list = list(tupl)
 tubl_list.append('dave')
-tubl_list.extend(['heelo', True]) # the extend method used to add a list not elemenst 
+tubl_list.extend(['hello', True])
 tupl = tuple(tubl_list)
-
 print(tupl)
 
-#   all the operation of lists can be used on tuple with the same methode 
-
-# unpacking a tuple into a variable is done like this 
-
-(var1 , var2 , *var3 ) = tupl # var1 will have the first data of the tuple var2 will ahve the second date var3 will have all of the reamining 
-
+# Unpacking a tuple into variables
+(var1, var2, *var3) = tupl
 print(var1)
 print(var2)
 print(var3)
-print(tupl)
 
-# count the occurences of a certain elemets
-print(tupl.count(2))  #one  argument is needed
-                       # it will count teh occurences of a given elemnt
+# Count occurrences of a certain element
+print(tupl.count(2))
 
 
-
-
-# dictionaries :are simular to lists and sets , but instead of and index it has a key for each elemet 
-#so a dict has a key and a value 
-
-##creating a dic 
-#direct methode
+# 4. Dictionaries: Key-value pairs
+# Define a dictionary
 music = {
     "lyrics": "by dave",
-    "instrumenst": "by ellys",
+    "instrument": "by ellys",
     "edit": "by person"
 }
 print(music)
-#the instructor methode
-band = dict(guitar="jake" , violon="ellys")
+
+# Using the constructor method
+band = dict(guitar="jake", violin="ellys")
 print(band)
 
-##acces items (both methodes need the key to access the item/value/elemnt)
-#direct methode
-print(band["guitar"])
+# Access items
+print(band["guitar"])  # Direct access
+print(band.get("guitar"))  # Using get method
 
-#the get methode
-print(band.get("guitar")) 
-
-##gett all keys
+# Get all keys
 print(band.keys())
 
-## get all items
-print(band.items)
-
-##list the keys and values in form of tuple pairs
-
+# Get all key-value pairs as tuples
 print(band.items())
 
-## verifie the existance of keys or item 
-#keys
+# Check for existence of keys
 print("guitar" in band)
-print("pino" in band)
-#items
-print("jake" in band) # becausse this methode is for keys
-#there is no function to do that i guess / sp it can be done by a loop wich will be coverde later
+print("piano" in band)
 
-## change or add value
-# direct methode only to change 
-band["guitar"] = "alex"
-print(band)
-# the update methode : add and change value
-band.update({"guitar": "alexener" , "pino": "brads" })
+# Add or change values
+band["guitar"] = "alex"  # Change value
+band.update({"piano": "brad"})  # Add new key-value pair
 print(band)
 
-##remove or delete items/dict
-# the pop methode 
-print(band.pop("violon")) # need an arguemnt
-print(band.popitem()) # need 0 argument to work # pop the last elemnt of dict
+# Remove items
+print(band.pop("violin"))  # Remove by key
+print(band.popitem())  # Remove the last inserted key-value pair
 print(band)
 
-band.update({"violon": "ellys"})
-band["pino"] = "moh"
-print(band)
-
-#the del methode 
-
-del band["pino"]
-print(band) 
-del music              
-#print(music) #this give an error message because music doesnt exist
-band.update({"violon": "ellys"})
-
-# emtiying a dict 
-music = dict(lyric="none" , instru="jaz")
-print(music)
+# Clear and delete a dictionary
+music = dict(lyric="none", instru="jazz")
 music.clear()
 print(music)
 
-# copy a dect and creating a reference 
-# refence methode
-band2 = band # when a change is applyed on band2 this vhange will also apply to band because both band and band2 refer to the same place in the memorie
-band2.pop("violon")
-print(band)
-band["piano"] = "ellys"
-del band2
-# creat a copy using the copy methode
- 
+# Copy a dictionary
 band2 = band.copy()
-
-band2.clear()
-print(band)
-print(band2)
-del band2
-#creat acopy using teh constructor methode 
-band2 = dict(band)
-band2.update({'piano': 'juba'})
-
+band2.update({'drums': 'mike'})
 print(band)
 print(band2)
 
 
-## nested dect 
-del band
+# Nested dictionaries
 member1 = {
     "name": "ellys",
-    "instrument": "piano" 
+    "instrument": "piano"
 }
-
 member2 = {
     "name": "dave",
     "instrument": "drums"
 }
-
 the_band = {
-    "fist": member1,
+    "first": member1,
     "second": member2
 }
-
 print(the_band)
-print(the_band["fist"]["name"])
+print(the_band["first"]["name"])
 
 
-## addition operations 
-# update can be used whith set dic tuples and lists
-    
-tuple1 = tuple((6,45,33))
-dict1 = {
-    'ell': "d"
-}
-ste = set((1,2,3))
-ste.update(tuple1)
-ste.update(dict1) # would only add the keys
-print(ste)
-
-# merge 2 set to creat new set
-set1 = set((1,2,3))
-set2 = {4, 3 ,6}
-
+# Sets - Additional operations
+# Merge two sets
+set1 = {1, 2, 3}
+set2 = {4, 3, 6}
 set3 = set1.union(set2)
 print(set3)
-# keep the duplictes from set1 and set2
+
+# Intersection (common elements)
 set4 = set1.intersection(set2)
 print(set4)
-# or without creating another set
-set1.intersection_update(set2)
-print(set1)
 
-# keep everythin exept the duplicates
-one = {1 , 2  ,3}
-set2.symmetric_difference_update(one)
+# Symmetric difference (elements not in both sets)
+set2.symmetric_difference_update(set1)
 print(set2)
